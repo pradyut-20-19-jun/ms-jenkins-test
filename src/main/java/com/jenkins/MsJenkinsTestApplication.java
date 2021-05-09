ea66c9a8e3bb39d800825927c23dbc3b16823344
pradyut-20-19-jun <49400349+pradyut-20-19-jun@users.noreply.github.com>
2021-05-09 21:31:16 +0530

    Update MsJenkinsTestApplication.java

--- src/main/java/com/jenkins/MsJenkinsTestApplication.java
+++ src/main/java/com/jenkins/MsJenkinsTestApplication.java
@@ -2,12 +2,19 @@ package com.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+ 
 @SpringBootApplication
-public class MsJenkinsTestApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MsJenkinsTestApplication.class, args);
-	}
+class MsJenkinsTestApplicationTests extends SpringBootServletInitializer{
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(MsJenkinsTestApplicationTests.class, args);
+    }
+ 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MsJenkinsTestApplicationTests.class);
+    }
 
 }
